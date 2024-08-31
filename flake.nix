fbac8178fd325b9ad1c24ea503206cba9c058b86
Kenneth <kennethnym@outlook.com>
2024-08-31 01:28:41 +0100

    chore: initial nix flake setup

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "A codegen tool for NanoPack";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?tag=24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          nanoc = pkgs.buildGoModule {
+            pname = "nanoc";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = "sha256-wyg35Xnw2TJirFCHX6DQY9OaeOBJf+xKnYvXk3AKzDU=";
+            buildInputs = [
+              # nanoc requires clang-format in clang-tools
+              pkgs.clang-tools
+              # nanoc uses biome to format typescript code
+              pkgs.biome
+              # nanoc uses swift-format to format swift code
+              pkgs.swift-format
+            ];
+          };
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.go
+              pkgs.gotools
+              # nanoc requires clang-format in clang-tools
+              pkgs.clang-tools
+              # nanoc uses biome to format typescript code
+              pkgs.biome
+              # nanoc uses swift-format to format swift code
+              pkgs.swift-format
+            ];
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.nanoc);
+    };
+}
